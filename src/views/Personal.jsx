d2a9404eb31bfdd1e64a6f9769b674c16715e0ea
Cgt <2635505189@qq.com>
2023-07-21 17:38:08 +0800

    feat：实现个人中心功能

--- src/views/Personal.jsx
+++ src/views/Personal.jsx
@@ -1,5 +1,82 @@
+import styled from "styled-components";
 import React from "react";
+import NavBarAgain from "../components/NavBarAgain";
+import { Link } from "react-router-dom";
+import { RightOutline } from "antd-mobile-icons";
+import { connect } from "react-redux";
+import action from "../store/action/index";
+import _ from "../assets/utils";
+import { Toast } from "antd-mobile";
+/* 样式 */
+const PersonalBox = styled.div`
+  .baseInfo {
+    box-sizing: border-box;
+    margin: 40px 0;
+    .pic {
+      display: block;
+      margin: 0 auto;
+      width: 172px;
+      height: 172px;
+      border-radius: 50%;
+    }
+    .name {
+      line-height: 100px;
+      font-size: 36px;
+      text-align: center;
+      color: #000;
+    }
+  }
+  .tab {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 30px;
+    height: 80px;
+    line-height: 80px;
+    font-size: 28px;
+    color: #000;
+    border-bottom: 2px solid #eee;
+  }
+`;
+const Personal = function Personal(props) {
+  let { info, clearUserInfo, clearStoreList, navigate } = props;
+  const signOut = () => {
+    //清除redux中的信息
+    clearUserInfo();
+    clearStoreList();
+    //清除token
+    _.storage.remove("tk");
+    //提示
+    Toast.show({
+      icon: "success",
+      content: "您已安全退出",
+    });
 
-export default function Personal() {
-  return <div>Personal</div>;
-}
+    //跳转
+    navigate("/login?to=/personal", { replace: true });
+  };
+  return (
+    <PersonalBox>
+      <NavBarAgain title="个人中心" />
+      <div className="baseInfo">
+        <Link to="/update">
+          <img className="pic" src={info.pic} alt="" />
+          <p className="name">珠峰培训</p>
+        </Link>
+      </div>
+      <div>
+        <Link to="/store" className="tab">
+          我的收藏
+          <RightOutline />
+        </Link>
+        <Link className="tab" onClick={signOut}>
+          退出登录 <RightOutline />
+        </Link>
+      </div>
+    </PersonalBox>
+  );
+};
+export default connect((state) => state.base, {
+  clearUserInfo: action.base.clearUserInfo,
+  clearStoreList: action.store.clearStoreList,
+})(Personal);
