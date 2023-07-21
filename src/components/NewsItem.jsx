4931ca03403d1e664c4e3b1c599240bc1f2e6e40
Cgt <2635505189@qq.com>
2023-07-21 17:44:43 +0800

    chore：新闻详情组件的完善

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -8,14 +8,14 @@ export default function NewsItem(props) {
   let { info } = props;
   if (!info) return null;
 
-  let { id, title, hint, images } = info;
-  if (!Array.isArray(images)) images = [""];
+  let { id, title, hint, images, image } = info;
+  if (!Array.isArray(images)) images = image ? [image] : [""];
   return (
     <div className="news-item-box">
       <Link to={{ pathname: `/detail/${id}` }}>
         <div className="content">
           <h4 className="title">{title}</h4>
-          <p className="author">{hint}</p>
+          {hint ? <p className="author">{hint}</p> : null}
         </div>
         <Image src={images[0]} lazy></Image>
       </Link>
