5135dbda0347dfdac623968d28b020ae93a9a11e
Cgt <2635505189@qq.com>
2023-07-23 23:27:03 +0800

    perf：去除打包时，生产map调试文件 （完结）

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -29,7 +29,8 @@ const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin"
 const createEnvironmentHash = require("./webpack/persistentCache/createEnvironmentHash");
 
 // Source maps are resource heavy and can cause out of memory issue for large source files.
-const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== "false";
+// const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== "false";
+const shouldUseSourceMap = "false"; //去除打包时，生成map打包文件，加快速度
 
 const reactRefreshRuntimeEntry = require.resolve("react-refresh/runtime");
 const reactRefreshWebpackPluginRuntimeEntry = require.resolve(
